6640dc8861f2088f8597f0547d59bf654f4cacb3
Daniel Cazzulino <daniel@cazzulino.com>
2022-11-19 12:02:11 -0300

    Initial sample extension that just provides a single Echo command
    And its corresponding EchoHandler and a File>Echo menu.

---
+++ Echo.cs
@@ -0,0 +1,26 @@
+﻿namespace EchoExtension;
+
+public partial record Echo(string Message) : ICommand<string>;
+
+[Shared]
+partial record class EchoHandler : ICommandHandler<Echo, string>
+{
+    public bool CanExecute(Echo command) => !string.IsNullOrEmpty(command.Message);
+
+    public string Execute(Echo command)
+        => string.IsNullOrEmpty(command.Message) ?
+            throw new NotSupportedException("Cannot echo an empty or null message") :
+            command.Message;
+}
+
+static partial class UI
+{
+    [MenuCommand("File.E_cho")]
+    public static async Task RunAsync(IMessageBus bus, IThreadingContext threading, CancellationToken cancellation)
+    {
+        await threading.SwitchToForeground(cancellation);
+        
+        if (MessageBox.Query("Echo", "Do you want to echo 'Hello World'?", "Obviously", "Nah") == 0)
+            MessageBox.Query("Echo", "We got back: " + bus.Execute(new Echo("Hello World")), "Ok");
+    }
+}
--- readme.md
+++ readme.md
@@ -1,3 +1,12 @@
 # Terminal.Shell EchoExtension
 
-A sample extension
+A sample extension with just one command.
+
+Simply copy this to your own project and use `IMessageBus` to execute it:
+
+```csharp
+using Merq;
+namespace EchoExtension;
+
+public partial record Echo(string Message) : ICommand<string>;
+```
